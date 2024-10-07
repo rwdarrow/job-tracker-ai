d825f14d3ea12aca35efb7a273db032dd49aac95
Robbie Darrow <rwdarrow3@gmail.com>
2024-10-07 00:58:41 -0400

    feat: add mantine

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,12 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+    importOrder: ['^@core/(.*)$', '^@server/(.*)$', '^@ui/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+    plugins: [
+        'prettier-plugin-tailwindcss',
+        '@trivago/prettier-plugin-sort-imports',
+    ],
 };
 
 export default config;
